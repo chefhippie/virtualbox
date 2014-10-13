f47362516021a150e4ec367ccc2a63c0d25bb077
Thomas Boerger <tboerger@suse.de>
2014-10-13 09:57:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+virtualbox Cookbook
+===================
---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name "virtualbox"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures virtualbox"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "virtualbox", "Installs/Configures virtualbox"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"

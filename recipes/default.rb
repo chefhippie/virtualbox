157bd2115734aa2a917a236fd6e77da0e5ef2afe
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:45:32 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["virtualbox"]["zypper"]["key"]
     title node["virtualbox"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["virtualbox"]["zypper"]["enabled"]
